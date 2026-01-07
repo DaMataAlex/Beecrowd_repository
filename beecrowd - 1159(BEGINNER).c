21e88d413184a55e6c0c2ffbda534a2baa411e9c
Alex Ribeiro <alex.rbribeiro@gmail.com>
2026-01-06 21:15:35 -0300

    beecrowd 1159

--- beecrowd - 1159(BEGINNER).c
+++ beecrowd - 1159(BEGINNER).c
@@ -2,6 +2,26 @@
 
 int main(){
 
-    
+    int cases;
+
+    while(1){
+        scanf("%d", &cases);
+        if(cases == 0){
+            break;
+        }
+
+        int total = 0;
+        int counter = 0;
+        while(counter != 5){
+            if((cases % 2) == 0){
+                total += cases;
+                counter++;
+            }
+            cases++;
+        }
+        printf("%d\n", total);
+        total = 0;
+    }
+
     return 0;
 }
