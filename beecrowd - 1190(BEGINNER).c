6f894cf4b9c8576208ee96dce64cf172c092c19d
Alex Ribeiro <alex.rbribeiro@gmail.com>
2026-02-08 09:30:11 -0300

    ex, 1189 & ex. 1190

--- beecrowd - 1190(BEGINNER).c
+++ beecrowd - 1190(BEGINNER).c
@@ -19,7 +19,7 @@ int main(){
     
     for(int i = 0; i < 12; i++){
         for(int j = 0; j < 12; j++){
-            if((i + j ) < 11 && (j < i)){
+            if((i + j ) > 11 && (j > i)){
                 count++;
                 total += array[i][j];
             }
